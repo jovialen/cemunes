63d817504e166662463dd3c302d51e6150c9e56c
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2023-09-14 10:44:55 +0200

    feat: implement start program opcodes

--- src/cpu.c
+++ src/cpu.c
@@ -4,12 +4,55 @@
 
 #include "bus.h"
 
-uint8_t fetch_op(cpu_t *cpu) {
+static uint8_t cpu_mem_read(cpu_t *cpu) {
   return bus_mem_read(cpu->bus, cpu->registers.pc++);
 }
 
+static void update_negative_zero_registers(cpu_t *cpu, uint8_t a) {
+  if (a == 0) {
+    cpu->registers.flags |= CPU_STATUS_FLAG_ZERO;
+  } else {
+    cpu->registers.flags &= ~CPU_STATUS_FLAG_ZERO;
+  }
+
+  if (a & CPU_STATUS_FLAG_NEGATIVE) {
+    cpu->registers.flags &= ~CPU_STATUS_FLAG_NEGATIVE;
+  } else {
+    cpu->registers.flags |= CPU_STATUS_FLAG_NEGATIVE;
+  }
+}
+
+static void lda(cpu_t *cpu) {
+  cpu->registers.a = cpu_mem_read(cpu);
+  update_negative_zero_registers(cpu, cpu->registers.a);
+}
+
+static void tax(cpu_t *cpu) {
+  cpu->registers.x = cpu->registers.a;
+  update_negative_zero_registers(cpu, cpu->registers.x);
+}
+
+static void inx(cpu_t *cpu) {
+  cpu->registers.x++;
+  update_negative_zero_registers(cpu, cpu->registers.x);
+}
+
+static uint8_t fetch_op(cpu_t *cpu) {
+  return cpu_mem_read(cpu);
+}
+
+const cpu_instruction_t INSTRUCTIONS[0xFF] = {
+  [0x00] = { .name = "brk", .func = 0    },
+  [0xa9] = { .name = "lda", .func = &lda },
+  [0xaa] = { .name = "tax", .func = &tax },
+  [0xe8] = { .name = "inx", .func = &inx },
+};
+
 void cpu_run(cpu_t *cpu) {
   for (uint8_t op = fetch_op(cpu); op != 0; op = fetch_op(cpu)) {
-    printf("%u: %x\n", cpu->registers.pc, op);
+    const cpu_instruction_t *instruction = &INSTRUCTIONS[op];
+    printf("%u (%s): %x\n", cpu->registers.pc, instruction->name, op);
+    instruction->func(cpu);
   }
+  printf("%u (%s): %x\n", cpu->registers.pc, INSTRUCTIONS[0].name, 0);
 }
--- src/cpu.h
+++ src/cpu.h
@@ -7,12 +7,12 @@
 
 
 typedef enum cpu_status_flag_t {
-	CPU_STATUS_FLAG_CARRY = 0,
-	CPU_STATUS_FLAG_ZERO,
-	CPU_STATUS_FLAG_INT_DISABLE,
-	CPU_STATUS_FLAG_DECIMAL,
-	CPU_STATUS_FLAG_OVERFLOW = 6,
-	CPU_STATUS_FLAG_NEGATIVE,
+	CPU_STATUS_FLAG_CARRY       = (1 << 0),
+	CPU_STATUS_FLAG_ZERO        = (1 << 1),
+	CPU_STATUS_FLAG_INT_DISABLE = (1 << 2),
+	CPU_STATUS_FLAG_DECIMAL     = (1 << 3),
+	CPU_STATUS_FLAG_OVERFLOW    = (1 << 6),
+	CPU_STATUS_FLAG_NEGATIVE    = (1 << 7),
 } cpu_status_flag_t;
 
 typedef uint8_t cpu_status_flags_t;
@@ -20,6 +20,7 @@ typedef uint8_t cpu_status_flags_t;
 typedef struct cpu_registers_t {
 	uint8_t x, y;
 	uint8_t s;
+	uint8_t a;
 	cpu_status_flags_t flags;
 	uint16_t pc;
 } cpu_registers_t;
@@ -28,6 +29,15 @@ typedef struct cpu_t {
 	cpu_registers_t registers;
 	bus_t *bus;
 } cpu_t;
+
+typedef void(*cpu_instruction_func_t)(cpu_t *);
+
+typedef struct cpu_instruction_t {
+	const char *name;
+	cpu_instruction_func_t func;
+} cpu_instruction_t;
+
+extern const cpu_instruction_t INSTRUCTIONS[0xFF];
 
 void cpu_run(cpu_t *cpu);
 
